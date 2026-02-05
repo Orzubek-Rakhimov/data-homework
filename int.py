0ddb5bcdd2b89945b0dfd032ea787454600fff73
Ismailov-Yusufbek <ismailovyusufbek014@gmail.com>
2026-02-05 21:07:19 +0500

    Ismailov Yusufbek tamonidan qilindi.

---
+++ int.py
@@ -0,0 +1,23 @@
+# 3 Ta float sonni butunga aylantiramiz.
+
+x=int(12.6)
+print(x,type(x))
+
+
+y=int(1451.9)
+print(y,type(y))
+
+
+i=int (76238762.7)
+print(i,type(i))
+
+
+# 2 Ta textni butun songa aylantiramiz.
+
+
+l=int("145678")
+print(l,type(l))
+
+
+o=int("14561")
+print(o,type(o))
