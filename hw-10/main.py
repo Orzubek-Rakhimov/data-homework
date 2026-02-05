d3f7f1a45336aaaed6d0e868ebdfd7a689f4b566
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-05 18:37:38 +0500

    Add examples of data type conversions and input handling
    This file contains commented-out code demonstrating various data type conversions and input handling in Python.

---
+++ hw-10/main.py
@@ -0,0 +1,62 @@
+# str()
+# int()
+# float()
+# bool()
+
+# input()
+
+# input("Yoshingizni kiriting: ")
+
+# yosh = input("Yoshingizni kiriting: ")
+
+# print("Sizning yoshingiz: ",yosh,"da")
+
+
+# son1 = int(input("Birinchi sonni kiriting:"))
+# son2 = int(input("Ikkinchi sonni kiriting:"))
+
+
+# type()
+# print(type(son1))
+# print(type(son2))
+
+# print(son1 + son2)
+
+# Xulosa input str tipindaki malumotlarni qaytaradi
+
+
+# print(int("on ikki"))
+
+
+# print(float(2))
+# print(type(float("2.23")))
+
+# print(type(str(10)))
+
+# print(type(bool(1)))
+
+
+# print(int(12.6))
+
+
+
+
+# 3 ta butun sonni flaotga aylantirmaiz va 2 ta textni floatga ayllantiramiz
+# 3 ta float sonni butunga aylantiramiz va 2 ta textni butun songa aylantiramiz
+# 2 ta butun sonni text ga aylantiramiz va 2 ta float sonni textga aylantiramiz va 1 ta boolean textga aylantiramiz
+# 4 ta input str , int , flaot , bool
+# son1 = str(input("Birinchi sonni kiriting:"))
+# print(son1,type(son1))
+# son1 = int(input("Birinchi sonni kiriting:"))
+# son1 = flaot(input("Birinchi sonni kiriting:"))
+# son1 = bool(input("Birinchi sonni kiriting:"))
+
+
+
+# x = float(12)
+# print(x , type(x))
+
+# y = float("134.34")
+# print(y , type(y))
+
+
