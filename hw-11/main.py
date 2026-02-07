21ee5a60462a971eb385899756b7a5c5fc3db46d
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-07 18:01:14 +0500

    Add comments for conversion and calculation functions
    Added comments for various conversion and calculation programs.

---
+++ hw-11/main.py
@@ -0,0 +1,4 @@
+# Kilometrdan metrga o'tuvchi programma duzamz.
+# Tortburchakni perimetri va yuzasini topadigan programm duzamz.
+# Sonning foizni chiqaradigan programma duzamz.
+# feet to meterga o'tuchi programma duzamz
