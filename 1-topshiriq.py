70c8380da6325dd37606b0a36cbda86439c866f4
Ismailov-Yusufbek <ismailovyusufbek014@gmail.com>
2026-02-12 21:34:52 +0500

    Homework-13 Ismailov Yusufbek tamonidan qilindi.

---
+++ 1-topshiriq.py
@@ -0,0 +1,11 @@
+# Foydalanuvchidan 4 ism olamiz 1 chi va 2 ism teng bolsa va  3 chi va 4 chi ism teng bolsa true deb chiqarsin.
+
+
+
+
+ism1=(input("1-Ismni kiriting :"))
+ism2=(input("2-Ismni kiriting :"))
+ism3=(input("3-Ismni kiriting :"))
+ism4=(input("4-Ismni kiriting :"))
+
+print(ism1==ism2 and ism3==ism4) 
---
+++ 2-topshiriq.py
@@ -0,0 +1,10 @@
+# foydalanuvchidan 4 ism olamiz xech bolmasa 1 juftlik teng bolishi kerak true deb chiqarsin
+
+
+
+ism1=(input("1-Ismni kiriting :"))
+ism2=(input("2-Ismni kiriting :"))
+ism3=(input("3-Ismni kiriting :"))
+ism4=(input("4-Ismni kiriting :"))
+
+print(ism1==ism2 or ism3==ism4) 
