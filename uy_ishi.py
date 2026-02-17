a5132c216354e49f6f3894df97171c7a399f3f2f
Ismailov-Yusufbek <ismailovyusufbek014@gmail.com>
2026-02-17 23:39:48 +0500

    Homework-16 Ismailov Yusufbek tamonidan qilindi.

--- uy_ishi.py
+++ uy_ishi.py
@@ -1,83 +1,173 @@
-# 1 Foydalanuvchidan input olinadi Toq va juft son topuchio programma tuzing.
 
-
-son=int (input("Sonni kirizing :"))
-if son % 2:
-    print("Bu son toq son !")
-else :
-    print("Bu son juft son !") 
+# Oson:
 
 
 
+ # 1.	Sonning musbat, manfiy yoki nol ekanligini tekshiradigan dastur yozing.
+# masala 
 
-
-
-
-# 2 Foydalanuvchidan input olinadi va bu input uning test dagi bahosi (0-100)
-# 90 va undan baland bolsa → "5"
-# 80–89 → "4"
-# 70–79 → "3"
-# 60–69 → "2"
-#  60 dan past → "1"
-
-
-baxo=int(input ("Testdan olgan baxoingizni kiriting :"))
-
-if  baxo<0 or baxo >100:
-    print("Xato son kiritingiz :")
-elif baxo>=90:
-    print("Sizning baxoingiz: 5 ")
-elif baxo>=80:
-   print("Sizning baxoinggiz: 4 ")
-elif baxo>=70:
-    print("Sizning baxoingiz: 3 ")
-elif baxo >= 60:
-    print("Sizning baxoingiz: 1 ")
+son=int(input("sonni kiriting :"))
+if son<0:
+    print(son, "siz manfiy son kiritingiz ")
 else:
-    print("Siz esi past lox odam ekansiz:")
+    print(son, "siz musbat son kiritingiz ")
 
 
 
+   
+    
+    
+#2	Agar yosh 18 ga teng yoki katta  bo'lsa → "Ovoz berish mumkin", aks holda "Juda yosh".
 
-
-
-
-
-# 3  Foydalanuvchidan 3 ta input olinadi  3 ta inputdan qaysi biri eng kattasi ekanligini chiqarsin
-# agar 3 tasi xam teng bolsa 3 tasi teng deb chiqarsin.
-
-
-son1=float(input("1-Sonni kiriting :"))
-son2=float(input("2-Sonni kiriting :"))
-son3=float(input("3-Sonni kiriting :"))
-
-if son1>son2 and son1>son3:
- print(son1, "Eng katta son")
-elif son2>son1 and son2> son3:
-    print(son2, "Eng katta son")
-elif son3>son1 and son3>son2:
-    print(son3, "Eng katta son")
-elif son1==son2 and son2==son3:
-    print("Xamma son teng !")
-
-
-
-
-
-
-
-
-# 4 Foydalanuvchidan 2 ta input olinadi (1-login , 2-kod)
-# kiritgan 2 ta inputi login va kod bn bir xil bo'lsa (kirish mumkin) bo'lmasa (kirish mumkin emas) dab chiqarsn.
-
-login="Ismailov"
-kod=123456789
-loginn=(input("Loginni kiriting :"))
-kodd=int(input("Kodinggizni kiriting :"))
-if login==loginn and kod==kodd:
-    print("Tizimga kirishingiz mumkun")
+yosh=int(input("Yoshingizni kiriting :"))
+if yosh>=18:
+    print("Sizga ovoz berish mumkun")
 else:
-    print("Kirishinggiz mumkun emas ! ")    
+    print("Siz yoshlik qilasiz ")
+
+
+    
+ 
+ 
+    
+#3 	2ta sonda eng kattasini chiqaradigan dastur yozing 
+
+son1=float(input("1 chi sonni kiriting"))
+son2=float(input("2 chi sonni kiriting"))
+if son1==son2:
+    print("Ikkala son teng ")
+elif son1>son2:
+    print("Birinchi son katta ")
+else:
+    print("Ikkinchi son katta ")
 
 
 
+
+
+
+# 4.	Foydalanuvchining bali 50 ball teng yoki undan yuqori → “O’tdingiz”, aks holda “O’ta olmadingiz”
+
+ball=int(input("Balingizni kiriting : "))
+if ball>=50:
+    print("Siz testdan otingiz ")
+else:
+    print("Siz testdan ota olmadingiz")
+
+
+
+
+#Ortacha
+
+ 
+
+# 1.	Foydalanuvchi 	kirizgan son 5 ga yoki 3 ga qoldiq siz bo’linadimi yo’qmi topilsin
+
+son=int(input("Sonni kirizing :"))
+if son % 5==0 and son % 3==0:
+    print("Bu son 3 ga va 5 ga bolinadi ")
+elif son %5==0 :
+    print("Son 5 ga bolinadi :")
+elif son%3==0 :
+    print("Bu son 3 ga bolinadi ")
+else:
+    print("Sonlar 3 ga xam 5 ga xam bolinmaydi ")
+
+
+
+
+
+
+
+
+# 2.	Agar tempratura 30 gradusdan katta bolsa issiq  va 15 gradusdan past bo’sa sovuq boshqa holatda iliq deb chiqarsin
+ 
+harorat=float(input("Haroratni kirizing : "))
+if harorat>=30:
+    print(harorat, "Harorat issiq")
+elif harorat<=15:
+    print(harorat, "Harorat sovuq")
+else:
+    print(harorat, "Harorat iliq")
+
+
+
+
+
+
+
+# 3.	Yil kabisa yoki kabisa yili emasligini chiqaruvchi dastur tuzing
+
+yil=int(input("Yilni kiriting : "))
+if yil % 400==0:
+    print(yil , "Yil kaaiba yil !")
+elif yil % 100==0:
+    print(yil ,"Yil kasiba yil emas !")
+elif yil % 4==0:
+    print(yil, "Yil kasiba yil !")
+else:
+    print(yil, "Yil kaaiba yil emas !")
+
+
+
+
+
+    
+    # 4.	Berilgan raqam (1-100) orasida yotadimi yoqmi tekshirilsin
+
+son=float(input("Son kiriting : "))
+if son>0 and son<=100:
+    print("Bu son 0-100 oraligida bor !")
+else:
+    print("Son 0-100 oraligida yoq ")
+
+
+
+
+
+# 5.	Kalkulyator zatish ( * ,  / , + , - ): 2 sonni ustida bajariladigan ammalar;
+
+
+son1=float(input("1-sonni kiriting : "))
+amallar=input("Amallarni tanlang :  + , - , * , /   :" )
+son2=float(input("2-sonni kiriting : "))
+if amallar=="+":
+     print(son1 + son2)
+elif amallar=="-":
+     print(son1 - son2)
+elif amallar=="*":
+      print(son1 * son2)
+elif amallar=="/":
+     if son2==0:
+          print("0 ga bolish mumkun emas !")
+     else:
+      print(son1 / son2)
+else:
+     print("Bu yerda xato bolish mumkun emas siz amalni xato qilgan bolishingiz mumkun buni yasash uchun 30 minut vaqt ketti !")
+
+
+
+
+
+# 6.	BMI natijasini chiqarish:
+# Foydalanuvchi kilosi va bo’yini grizishi va vnda galib chiqib bmi chiqarish:
+# yengil:  18.5 kichkina
+# Normal : 18.5–24.9
+# semiz: 25.0–29.9
+# juda semizroq: 30.0 yoki baland 
+
+
+boy=float(input("Boyingizni kiriting : "))
+vazn=float(input("Vazningizni kiriting : "))
+kv_boy=(boy**2)
+bmi=vazn/kv_boy
+print(bmi, "Bu sizning BMI natijangiz !")
+if bmi<18.5 :
+     print("Sizning BMI darajangiz Yengil ! ")
+elif bmi>=18.5 and bmi<=24.9:
+     print("Sizning BMI darajangiz Normal")
+elif bmi >= 25.0 and bmi<=29.9:
+     print("Sizning BMI darajangiz Semiz")
+elif  bmi>=30:
+     print("Sizning BMI darajangiz Juda semiz")
+     
