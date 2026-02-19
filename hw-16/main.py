f2a096bb7c4d0f0b412b3ff9a8df03919253f292
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-19 18:35:21 +0500

    Implement user input and calculation functionalities
    Added a Python script that prompts the user for numbers and performs various calculations, including products and averages.

---
+++ hw-16/main.py
@@ -0,0 +1,36 @@
+
+# 1
+# Foydalanucgidan son so'raladi va shu sonni 1 dan 10 gacha bo'lgan sonlar ko'paytmasi chiqariladi
+# input => 20 | 30
+# 20 | 30
+# 40  | 60
+# 60  | 90
+# ...
+# 180 | 270
+# 200 | 300
+
+
+# 2
+# Foydalanuvchidan son lar so'ralavradi qachongi foydalanuchi manfiy son kirgizguncha va bundan keyin oldingi kirgizilgan sonlar ko'paytmasi chiqarilsin
+#  10
+# 50 
+# 100
+# 5
+# 2 
+# -1 | -23
+
+# 10 * 50 * 100 * 5 * 2 = 500_000
+
+
+# 3
+# Foydalanuvhcidan 1dan 100gacha son kirgizish so'raladi (xoxlagancha) qachonki foydalanuvchi "stop"
+# deb yozsa o'shanda programa to'xtashi va
+# kirgizilgan sonlarning o'rta qiymatini chiqarishi kerak
+# 1 - 100
+# "stop"
+# 5
+# 10
+# 6
+# 3
+# stop
+# (5 + 10 + 6 + 3 ) / 4 = 6
