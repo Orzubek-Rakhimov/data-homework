8a21c706145c33ad470a5d6e77d5ffe09b2cb84a
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-02-21 16:30:36 +0500

    Implement user input calculations and average output
    Added functionality for user input and calculations including multiplication table, product of numbers, and average of valid inputs.

--- hw-16/main.py
+++ hw-16/main.py
@@ -22,6 +22,25 @@
 # 10 * 50 * 100 * 5 * 2 = 500_000
 
 
+
+# 2 chini javobi
+son = int(input("Sonni kiritng: "))
+n = 1
+while n <= 10 : 
+    print(son * n) 
+    n = n + 1
+  
+  
+  # 10  
+son = int(input("Sonni kiriting:"))
+kopaytma = 1
+while son > -1 : 
+    kopaytma = kopaytma * son 
+    son = int(input("Sonni kiriting:")) 
+print(kopaytma)
+
+
+
 # 3
 # Foydalanuvhcidan 1dan 100gacha son kirgizish so'raladi (xoxlagancha) qachonki foydalanuvchi "stop"
 # deb yozsa o'shanda programa to'xtashi va
@@ -34,3 +53,25 @@
 # 3
 # stop
 # (5 + 10 + 6 + 3 ) / 4 = 6
+
+
+# 3 chini javobi
+# "10"
+son_yoki_stop = input("Sonni kiring: ")
+yigindi = 0
+k = 0
+
+while son_yoki_stop != "stop":
+    # 10
+    son = int(son_yoki_stop)
+    
+    
+    if 1 <= son and son <= 100:
+        yigindi = yigindi + son
+        k = k + 1
+    else:
+        print("Sonni to'g'ri kiriting!")
+        
+    son_yoki_stop = input("Sonni kiring: ")
+
+print( yigindi / k)
