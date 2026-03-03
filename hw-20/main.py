472ee87fb683383d05c21b11d0588817e6acc43a
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-03-03 16:14:00 +0500

    Add new programming tasks and exercises
    Added new programming tasks and exercises for users, including functions for calculating squares, factorials, prime numbers, and more.

--- hw-20/main.py
+++ hw-20/main.py
@@ -1,22 +1,40 @@
-Oson
-1. Kvadratlar jadvali: 1 dan 20 gacha bo'lgan sonlarning kvadratini (i**2) ekranga jadval ko'rinishida chiqaring.
-2. Foydalanuvchidan son so'raymiz va shu sondan 1 ga cha bolgan sonlarni chiqaramiz ( 100 => 100,99,98 ....1) ( 5 => 5,4,3,2,1)
-O'rtacha
-3.  Foydalanuvchidan son so'ralsi9n va shu sonni 1 dan 10 gacha ko'paytmasi chiqarilsin ( 5 => 5 * 1 = 5, 5 * 2 = 10 , 15,20 .... 45,50) ( 7 => 7,14,21,28 ... 63,70)
-4.   Faktorialni topish: Berilgan musbat butun sonning faktorialini (masalan, 5! = 1*2*3*4*5) for yordamida hisoblaydigan dastur tuzing 
-Qiyin
-5. Tub sonlarni aniqlash: 1 dan 100 gacha bo'lgan oraliqdagi barcha tub sonlarni (faqat o'ziga va 1 ga bo'linadigan sonlar) topib chiqaring  (2,3,5,7 ... 97)
-6. Yulduzchali piramida: Ichma-ich tsikllar yordamida quyidagi shaklni hosil qiling (int)
-=> (5)
-*
-**
-***
-****
-*****
-=> (6)
-*
-**
-***
-****
-*****
-******
+1.  Juft sonlar yig'indisi: foydalanuvchi son kirgizadi shu songa gacha bo'lgan barcha juft sonlarni qo'shib, umumiy natijani hisoblang.
+2.  Takrorlash: Foydalanuvchidan biror so'z va biror son (masalan, 5) kiritishni so'rang. Tsikl yordamida o'sha so'zni kiritilgan soncha marta chiqaring.
+assalom x assalom x assalom x
+3.  Dollar kursi: 1 dan 10 gacha bo'lgan dollarning so'mdagi qiymatini jadval ko'rinishida chiqaring (masalan: "1 = 25200 so'm").
+
+      4.Dollar kursi:1 dan foydalanuvcgi kirgizgan songacha gacha bo'lgan dollarning so'mdagi qiymatini jadval ko'rinishida chiqaring (masalan: "1 = 25200 so'm").
+Uyga vazifa:
+1.  Karra jadvali: Faqat 10 soni uchun karra jadvalini (7x1 dan 7x10 gacha) to'liq chiqaring.
+1X1=1
+1x2=2
+…….
+
+
+
+9 x 1 = 9
+9 x 2 =18
+
+
+…
+
+9x9=90
+
+2) 1X1=1  2x1=2 3x1=3 … 9x1=9
+1x2=2 2x2=4 3x2=6 … 9x2=18
+…
+1x9=9 2x9=18 3x9=27 … 9x9=81
+
+
+1.  Salom, Dunyo!: Ekranga "Men Pythonni o'rganyapman" jumlasini tsikl orqali 20 marta chiqaring. (13 siklda chiqarmasn va 19 siklda toxtatsin)
+2.  Sonlarni o'tkazib yuborish: 1 dan 100 gacha bo'lgan sonlarni chiqaring, lekin 5 ga bo'linadigan sonlar kelganda ularni tashlab o'tib keting (continue ishlating)
+3.  Koordinatalar: 2 ta son so’rimiz va shu sonlar gacha bo'lgan x va y o'qlari uchun barcha nuqtalarni chiqaring (masalan: (0,0), (0,1), (0,2)...).
+(nested loop) (2 forni bir birini ichida ishlatish)
+     
+1.    Hard:
+Matritsa: 3x3 o'lchamdagi matritsani (ro'yxat ichidagi ro'yxat) ekranga chiroyli shaklda chiqaring.
+
+
+
+
+20-dars uyga vazifa
