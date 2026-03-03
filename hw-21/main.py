4d87a6ec376e9961c4bad65a799ebaa5c1d0ab7a
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-03-03 18:36:48 +0500

    Add string manipulation tasks in main.py
    Added Python programming tasks related to string manipulation and character counting.

---
+++ hw-21/main.py
@@ -0,0 +1,12 @@
+1.Foydalanuvchi login kiritayotganda tasodifan boshiga yoki oxiriga bo'sh joy (space) tashlab ketsa, bu xatoni qanday bartaraf etasiz?
+"***Python***" satridagi yulduzchalarni olib tashlab, faqat so'zning o'zini qoldirish uchun ushbu metoddan qanday foydalaniladi?
+
+
+2.1 Berilgan gapdagi barcha nuqtalarni vergulga almashtiruvchi kod yozing.
+2.2 Satr ichidagi bo'sh joylarni (probel) butunlay olib tashlash (ya'ni "" ga almashtirish) uchun bu metodni qanday qo'llaysiz?
+
+3.1 Matn ichida "Python" so'zi necha marta takrorlanganini aniqlang.
+3.2 Satrdagi unli harflar (masalan, 'a') sonini hisoblashda ushbu metoddan qanday foydalaniladi?
+
+4 Berilgan matndagi barcha tinish belgilarini olib tashlab, so'ngra barcha so'zlarni katta harfga o'tkazuvchi va a harfini o ga aylantiruvchi dastur tuzing. Bunda ham for, ham yuqoridagi metodlardan foydalaning.
+"g'afur" => "GOFUR"
