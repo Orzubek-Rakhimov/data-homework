3faf0064dcf4b884c9cc3ae8b8d258fcc912267d
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-03-05 18:49:44 +0500

    Create main.py

---
+++ hw-22/main.py
@@ -0,0 +1,9 @@
+1.Berilgan gapdagi barcha raqamlarni olib tashlang, faqat harflar qolsin.
+2.Foydalanuvchi kiritgan so‘z palindrom ekanligini tekshiring.
+3. Matndagi ortiqcha bo‘shliqlarni bittaga aylantiring. ("Assalom    aleykum  aasda" => "Assalom aleykum aasda")
+4. So‘zlar ichidagi unli harflarni “*” belgisi bilan almashtiring.
+5. Berilgan gapdagi so‘zlarni teskari tartibda joylashtiring, lekin har bir so‘z o'rni o‘zgarishsiz qolsin.
+Masalan:
+  # "Assalom aleykum yurtdoshim"
+  # "mishodtruy mukyela molassA" => xato bunday emas
+  # "molassA mukyela mishodtruy" => to'gri
