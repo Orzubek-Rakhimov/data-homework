0a406a31b00753a0a398df6d2d332ad020084316
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-03-07 18:43:36 +0500

    Create main.py with function specifications
    Added a new Python file with various function specifications for basic programming tasks.

---
+++ hw-23/main.py
@@ -0,0 +1,12 @@
+Oson daraja (Easy)
+1.Kvadrat hisoblovchi: Son qabul qilib, uning kvadratini konsolga chiqaruvchi funksiya yozing.
+2.Yosh hisoblagich: Tug'ilgan yilni qabul qilib, foydalanuvchining yoshini hisoblab beruvchi funksiya yarating.
+3.Juft yoki toq: Berilgan son juft bo'lsa True, toq bo'lsa False qaytaruvchi funksiya yozing.
+4.Eng kattasi: Ikkita son qabul qilib, ulardan kattasini ekranga chiqaruvchi funksiya tuzing.
+5.Text uzunligi: Text qabul qilib, unda uzunligini chiqaruvchi (tayyor len() dan foydalanmaslikka harakat qiling).
+
+O'rta daraja (Medium)
+Unli harflar: Matn qabul qilib, undagi unli harflar sonini hisoblab qaytaruvchi funksiya yozing.
+
+Parol tekshirgich: Parol (string) qabul qilib, uning uzunligi kamida 8 ta belgidan iboratligini tekshiruvchi funksiya yozing.
+Standart qiymat: Foydalanuvchi ismi so'ralsin va funksiya orqali ("Salom , {ism}) chiqarilsin!
