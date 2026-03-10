eefedefba04b914b8cba63204e7e36c223f4c441
Rahimov Orzubek <145760665+Orzubek-Rakhimov@users.noreply.github.com>
2026-03-10 18:38:45 +0500

    Add new functions for name handling and currency conversion

---
+++ hw-24/main.py
@@ -0,0 +1,3 @@
+1.Ism-sharif: toliq_ism funksiyasini yarating. U ism va familiya qabul qilsin, lekin familiya kiritilmasa, u standart bo'sh matn "" bo'lsin. Funksiya birlashtirilgan matnni qaytarsin.
+2.Valyuta konvertori: kurs_hisobla funksiyasi miqdor va kurs (standart qiymati 12600) qabul qilsin. Funksiya so'mga aylantirilgan qiymatni qaytarsin.
+3.Do'kon cheki: hisob_kitob funksiyasi mahsulot, narh, soni va chegirma (standart 0) parametrlarini qabul qilsin. Argumentlarni uzatishda ularning tartibini aralashtirib yuboring (masalan, birinchi chegirma, keyin mahsulot). Funksiya jami summani matn ko'rinishida qaytarsin.
